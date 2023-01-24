a26340a92163c3aeff7f8b4855945ba13ffa1ac9
ars3232 <36531986+ars3232@users.noreply.github.com>
2023-01-24 13:57:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,111 @@
                </a>
              </div>
            </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/02.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/03.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/04.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/05.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/06.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/07.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/08.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/09.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/10.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/11.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/12.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/13.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/14.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/15.jpg" alt="">
+               </a>
+             </div>
+           </div>
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/16.jpg" alt="">
+               </a>
+             </div>
+           </div>
        </div>
      </div>
 
