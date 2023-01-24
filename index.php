9706a3133e755a01e740ba3042e738395f277ad5
ars3232 <36531986+ars3232@users.noreply.github.com>
2023-01-24 13:50:28 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<title>Ecommerce Bishkek</title>
+  </head>
+  <body
+        <div class="wrapper">
+         <div class="gallery">
+           <div class="items">
+             <div>
+               <a href="img/big/01.jpg">
+                 <img class="front" src="img/small/01.jpg" alt="">
+               </a>
+             </div>
+           </div>
+       </div>
+     </div>
+
+              
+  </body>
+  </html>
